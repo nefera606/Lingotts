3aede814a45689de97dbb2380951a5862736cd6e
DESKTOP-HUQVG7V\Enrique Alcázar <enriqueag26@gmail.com>
2017-12-09 21:07:36 +0100

    refactor file structure

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var Attributes = artifacts.require("./Attributes.sol");
-var Multiowned = artifacts.require("./Multiowned.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-};
