f60d360c125cf4bf4652289cab4431dc45582b1b
Jorge disRupt <jm@disruptavila.com>
2017-12-09 23:23:34 +0800

    Update token-ERC20.sol

--- token-ERC20.sol
+++ token-ERC20.sol
@@ -1,3 +1,5 @@
+/// @title Standard Token Contract.
+
 pragma solidity ^0.4.16;
 
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
