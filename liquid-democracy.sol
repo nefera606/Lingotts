79ae40929307d9047980c942bfd4168178e1ae41
Jorge disRupt <jm@disruptavila.com>
2017-12-09 18:59:45 +0800

    Update liquid-democracy.sol

--- liquid-democracy.sol
+++ liquid-democracy.sol
@@ -1,7 +1,15 @@
 pragma solidity ^0.4.16;
 
 contract token {
-    mapping (address => uint256) public balanceOf;
+    mapping (address => uint256) public voteWeight;
+    uint public numberOfDelegationRounds;
+
+    function balanceOf(address member) constant returns (uint256 balance) {
+        if (numberOfDelegationRounds < 3)
+            return 0;
+        else
+            return this.voteWeight(member);
+    }
 }
 
 contract LiquidDemocracy {
